3dbef4313c60f328ca5159b85c87a6408bb28f4f
Amar <sharewithamar@gmail.com>
2017-06-08 10:01:21 +0530

    config for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port= process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -64,6 +65,6 @@ app.get('/bad', (req, res) => {
         errorMessage: `Avv that's bad url..`
     });
 });
-app.listen(3000, () => {
-    console.log('Server is up');
+app.listen(port, () => {
+    console.log(`Server is up in ${port}`);
 });
